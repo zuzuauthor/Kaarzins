0d5e7970aab72de08e60e46df43deb4d5cbb0b59
Arvind <akyadav020190@gmail.com>
2018-07-29 17:47:09 +0530

    Checking for push

--- app/src/main/java/zuzusoft/com/kaarzins/splash/SplashActivity.java
+++ app/src/main/java/zuzusoft/com/kaarzins/splash/SplashActivity.java
@@ -1,9 +1,12 @@
 package zuzusoft.com.kaarzins.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import zuzusoft.com.kaarzins.R;
+import zuzusoft.com.kaarzins.getstart.LoginActivity;
 import zuzusoft.com.kaarzins.helper.BaseActivity;
 
 
@@ -13,10 +16,48 @@ import zuzusoft.com.kaarzins.helper.BaseActivity;
 
 public class SplashActivity extends BaseActivity {
 
+    private final int SPLASH_TIME = 2500;
+    private Runnable runnable;
+    private Handler handler;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showSplashScreen();
+    }
+
+    private void showSplashScreen() {
+        runnable = new Runnable() {
+            public void run() {
+                Intent intent;
+                /*if (AppPreferences.isLoggedIn(SplashActivity.this)) {
+                    intent = new Intent(getApplicationContext(), MainActivity.class);
+                } else {
+                    intent = new Intent(getApplicationContext(), LoginActivity.class);
+                }*/
+                intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler.postDelayed(runnable, SPLASH_TIME);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        if (runnable != null)
+            handler.removeCallbacks(runnable);
+    }
+
 }
